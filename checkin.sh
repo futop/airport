404a9a516acca7a2230660c6dff0291221b31fdb
Yun <fuyz1993@gmail.com>
2021-01-03 15:37:21 +0800

    Create checkin.sh

---
+++ checkin.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+curl 'https://www.wiougong.space/user/checkin' \
+  -X 'POST' \
+  -H 'authority: www.wiougong.space' \
+  -H 'content-length: 0' \
+  -H 'sec-ch-ua: "Google Chrome";v="87", " Not;A Brand";v="99", "Chromium";v="87"' \
+  -H 'accept: application/json, text/javascript, */*; q=0.01' \
+  -H 'x-requested-with: XMLHttpRequest' \
+  -H 'sec-ch-ua-mobile: ?0' \
+  -H 'user-agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36' \
+  -H 'origin: https://www.wiougong.space' \
+  -H 'sec-fetch-site: same-origin' \
+  -H 'sec-fetch-mode: cors' \
+  -H 'sec-fetch-dest: empty' \
+  -H 'referer: https://www.wiougong.space/user' \
+  -H 'accept-language: zh-CN,zh;q=0.9,en;q=0.8' \
+  -H 'cookie: __cfduid=dee91a3442f1df2edb7d9be2ecfd9773f1607581097; lang=zh-cn; _ga=GA1.2.146386129.1607862533; uid=22107; email=fuyz1993%40gmail.com; key=eafe2b5944526eb85bfa274184849d7684cce83a8696b; ip=ec23b52c78662421a106d17310fea33a; expire_in=1609745717' \
+  --compressed
