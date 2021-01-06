163efe8ff31adf4eba224f87b1ac64309b6b9f7b
Yun <fuyz1993@gmail.com>
2021-01-06 18:16:10 +0800

    Update aqi.sh

--- aqi.sh
+++ aqi.sh
@@ -13,10 +13,9 @@ station=$(echo ${station#\"}) && station=$(echo ${station%\"})
 quality=$(echo ${quality#\"}) && quality=$(echo ${quality%\"})
 pollutant=$(echo ${pollutant#\"}) && pollutant=$(echo ${pollutant%\"})
 
-title="Server醬"
-content="变态 目前 由 $station 监测到的的空气质量指数是:$aqi 属于 $quality 主要污染物是 $pollutant
-才 才不是特意提醒你的呢 哼~
-![face](https://dn-4679kun.qbox.me/691875be0ff27db2.png)"
+title="Server醬温馨提醒您"
+content="目前 由 $station 监测到的的空气质量指数是:$aqi 属于 $quality 主要污染物是 $pollutant
+才 才不是特意提醒你的呢 哼~"
 
 echo "[$(date)]" >> $HOME/aqi.log
 curl -s "http://sc.ftqq.com/$key.send?text=$title" -d "&desp=$content" > $HOME/aqi.log
