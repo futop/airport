45468a9c5ce4bc36263d1a90104c44b8286ec0a7
Yun <fuyz1993@gmail.com>
2021-01-10 00:42:38 +0800

    Update checkin.sh

--- checkin.sh
+++ checkin.sh
@@ -16,3 +16,21 @@ curl 'https://www.wiougong.space/user/checkin' \
   -H 'accept-language: zh-CN,zh;q=0.9,en;q=0.8' \
   -H 'cookie: __cfduid=dee91a3442f1df2edb7d9be2ecfd9773f1607581097; lang=zh-cn; _ga=GA1.2.146386129.1607862533; uid=22107; email=fuyz1993%40gmail.com; key=eafe2b5944526eb85bfa274184849d7684cce83a8696b; ip=ec23b52c78662421a106d17310fea33a; expire_in=1609745717' \
   --compressed
+  
+  curl 'https://v2.freeyes.xyz/user/checkin' \
+  -X 'POST' \
+  -H 'authority: v2.freeyes.xyz' \
+  -H 'content-length: 0' \
+  -H 'sec-ch-ua: "Google Chrome";v="87", " Not;A Brand";v="99", "Chromium";v="87"' \
+  -H 'accept: application/json, text/javascript, */*; q=0.01' \
+  -H 'x-requested-with: XMLHttpRequest' \
+  -H 'sec-ch-ua-mobile: ?0' \
+  -H 'user-agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36' \
+  -H 'origin: https://v2.freeyes.xyz' \
+  -H 'sec-fetch-site: same-origin' \
+  -H 'sec-fetch-mode: cors' \
+  -H 'sec-fetch-dest: empty' \
+  -H 'referer: https://v2.freeyes.xyz/user' \
+  -H 'accept-language: zh-CN,zh;q=0.9,en;q=0.8' \
+  -H 'cookie: __cfduid=d7070e0251bec6af15187564a66cab44b1609065424; uid=491852; email=1530589998%40qq.com; key=c64373be676e21a247280f701cd1441ca9a46c962489c; ip=ba6643dd22360b8e80d6cc8423f30a63; expire_in=1610288104' \
+  --compressed
